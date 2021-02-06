17b487a31651038000aab2c503ec5a2a2e76b9d7
Eric Ervin <ericcervin@gmail.com>
2021-02-06 00:03:33 -0500

    added rakdos_cards output

--- magic.py
+++ magic.py
@@ -1,26 +1,60 @@
 import json
 
-#files from https://mtgjson.com/
-set_files = ['KHM.json']
-for set_json in set_files:
-    with open("./resources/magic/IN/" + set_json, "r") as read_file:
-        data = json.load(read_file)
+def main():
+    #files from https://mtgjson.com/
+    sets = ['KHM','M21','ZNR','IKO','THB','ELD']
+    for card_set in sets:
+        with open("./resources/magic/IN/" + card_set + ".json", "r") as read_file:
+            data = json.load(read_file)
+            set_name = data['data']['name']
+            total_set_size = data['data']['totalSetSize']
+            base_set_size = data['data']['baseSetSize']
+            brief_card_maps = map(brief_card_map, data['data']['cards'])
+            distinct_cards = {}
+            distinct_rakdos_cards = {}
+            for c in brief_card_maps:
+                if distinct_cards.has_key(c['name']):
+                    distinct_cards[c['name']].append(c)
+                else:
+                    distinct_cards[c['name']] = [c]
+                if c['is_rakdos']:
+                  if distinct_rakdos_cards.has_key(c['name']):
+                      distinct_rakdos_cards[c['name']].append(c)
+                  else:
+                      distinct_rakdos_cards[c['name']] = [c]
+            with open("./resources/magic/OUT/all_cards_" +  card_set + ".txt", "w") as all_card_file:
+                 for dc in distinct_cards.keys():
+                     text = brief_card_text(distinct_cards[dc][0])
+                     all_card_file.write(text)
+            with open("./resources/magic/OUT/rakdos_cards_" + card_set + ".txt", "w") as rakdos_card_file:
+                 for dc in distinct_cards.keys():                     
+                     if distinct_cards[dc][0]['is_rakdos']:
+                        text = brief_card_text(distinct_cards[dc][0])
+                        rakdos_card_file.write(text)
+            rakdos_card_count = len(distinct_rakdos_cards)
+            print(set_name + '\t' + str(total_set_size) + '\t' + str(base_set_size) + '\t' + str(rakdos_card_count))
 
-        #print(type(data))
-        #print(data.keys())
-        #print(data['data'].keys())
-        set_name = data['data']['name']
-        #print(type(data['data']))
-        #print(data['data'].keys())
-        #print(type(data['data']['cards']))
-        total_set_size = data['data']['totalSetSize']
-        print(set_name + '\t' + str(total_set_size))
-        #print(data['data']['cards'][0].keys())
-        #print(data['data']['cards'][0])
-        #print(data['data']['cards'][0]['text'].encode("utf-8"))
-        with open("./resources/magic/OUT/allcards.txt", "w") as allcard_file:
-          for i in data['data']['cards']:
-              card_name = i.get('name','')
-              card_number = i.get('number','')
-              card_color_id = i.get('colorIdentity','')
-              allcard_file.write(card_number.encode("utf-8") + '\t' + card_name.encode("utf-8") + '\t' + (' ').join(card_color_id) + '\n')
+def isRakdos(color_id):
+    return (not (('U' in color_id) or ('W' in color_id) or ('G' in color_id)) )
+
+def brief_card_map(card_json):
+   return {'set_code': card_json.get('setCode',''),
+           'name':  card_json.get('name',''),
+           'type': card_json.get('type',''),
+           'number': card_json.get('number',''),
+           'color_id': card_json.get('colorIdentity',''),
+           'is_rakdos': isRakdos(card_json.get('colorIdentity','')),
+           'is_starter': card_json.get('isStarter','')
+          }
+    
+def brief_card_text(card_map):
+    return ('\t').join([
+                 card_map['set_code'],
+                 card_map['number'],
+                 card_map['name'],
+                 #card_map['type'].encode("ascii","ignore"),
+                 (',').join(card_map['color_id']),
+                 #str(card_map['is_rakdos'])
+                 ]) + '\n'
+
+main()    
