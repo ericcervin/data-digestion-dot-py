c0abb43b433de28283709cef295a3cda8f7da090
Eric Ervin <ericcervin@gmail.com>
2021-02-16 17:33:02 -0500

    added subtype_counts.txt output

--- magic.py
+++ magic.py
@@ -2,7 +2,7 @@
 
 import json
 
-sets = ['GRN','RNA','WAR','M20','ELD','THB','IKO','M21','ZNR','KHM']
+sets = ['AKH','HOU','XLN','RIX','DOM','M19','GRN','RNA','WAR','M20','ELD','THB','IKO','M21','ZNR','KHM']
 
 def main():
     #files from https://mtgjson.com/
@@ -17,6 +17,8 @@ def main():
         base_set_size = set_data[k]['base_set_size']
         distinct_card_count = len(set_data[k]['distinct_cards'])
 
+        set_data[k]['subtype_counts'] = subtype_counts(set_data[k]['distinct_cards'])
+
         distinct_rakdos_cards = {key:value for (key,value) in set_data[k]['distinct_cards'].items() if value[0]['is_rakdos'] }
         distinct_rakdos_card_count = len(distinct_rakdos_cards)
 
@@ -28,10 +30,18 @@ def main():
         file_name = "rakdos_cards_" + set_id + ".txt"
         print_card_tsv(file_name,distinct_rakdos_cards)
 
+    with open("./resources/magic/OUT/subtype_counts.txt", "w") as card_file:
+             for k in sets:
+               for st in set_data[k]['subtype_counts'].items():
+                 card_file.write(k + '\t' + st[0] + '\t'+ str(st[1]) + '\n')
+
 def brief_card_map(card_json):
    return {'set_code': card_json.get('setCode',''),
            'name':  card_json.get('name',''),
            'type': card_json.get('type',''),
+           'types': card_json.get('types',''),
+           'subtypes': card_json.get('subtypes',''),
+           'supertypes': card_json.get('supertypes',''),
            'number': card_json.get('number',''),
            'color_id': card_json.get('colorIdentity',''),
            'is_rakdos': isRakdos(card_json.get('colorIdentity','')),
@@ -45,7 +55,7 @@ def brief_card_text(card_map):
                  card_map['number'].replace('\u2605',''),
                  card_map['name'],
                  str(card_map['cmc']),
-                 #card_map['type'].encode("ascii","ignore"),
+                 card_map['type'].encode("ascii","ignore"),
                  (',').join(card_map['color_id']),
                  #str(card_map['is_rakdos'])
                  ]) + '\n'
@@ -59,7 +69,6 @@ def distinct_card_maps(cards):
                 else:
                     distinct_cards[c['name']] = [c]
     return distinct_cards
-
 
 def isRakdos(color_id):
     return (not (('U' in color_id) or ('W' in color_id) or ('G' in color_id)) )
@@ -77,12 +86,19 @@ def load_set_data(sets):
                                   } 
     return set_data
 
-
 def print_card_tsv(file_name,card_map):   
     sorted_cards = sorted(card_map.items(), key=lambda x: x[1][0]['name'])
-    #print(sorted_cards[0])
     with open("./resources/magic/OUT/" + file_name, "w") as card_file:
-             for c in sorted_cards:                     
+             for c in sorted_cards:
+               #print(c[0] + '\t' + str(len(c[1])))
                text = brief_card_text(c[1][0]).encode('utf-8')
                card_file.write(text)
+
+def subtype_counts(card_map):
+  subtype_counts = {}
+  for c in card_map.values():
+    for st in c[0]['subtypes']:
+            subtype_counts[st] = subtype_counts.get(st,0) + 1
+  return subtype_counts
+               
 main()    
